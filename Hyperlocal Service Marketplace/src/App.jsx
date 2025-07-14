5b6f7b660c0b0820070f3cb331f1fd5b9d4b0baa
Karmanya Pandey <karmanyapandey348@gmail.com>
2025-07-14 22:53:47 +0530

    navbar updated, login and register page created

--- Hyperlocal Service Marketplace/src/App.jsx
+++ Hyperlocal Service Marketplace/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Services from './pages/Services';
+import Login from './pages/Login'; 
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
