5c4f20a3abcd366758694ab187eb7fa3769b22e8
shubhangi0404 <shubhangisrivastava237@gmail.com>
2025-07-15 18:36:29 +0530

    add booking and payment page

--- Hyperlocal Service Marketplace/src/App.jsx
+++ Hyperlocal Service Marketplace/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Services from './pages/Services';
+import BookingPage from './pages/BookingPage';
+import PaymentPage from './pages/Payment'; 
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/booking" element={<BookingPage />} /> 
+        <Route path="/payment" element={<PaymentPage />} />  
       </Routes>
     </>
   );
